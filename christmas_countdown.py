410fff054f2bc97d96a25ddcee4a9a1aeb4ae85f
Brandon Nolan <brandonnolan3123@gmail.com>
2020-09-17 14:54:04 -0600

    Better....I was using a 'date' instead of 'datetime'.....rookie!

--- christmas_countdown.py
+++ christmas_countdown.py
@@ -1,4 +1,5 @@
 from datetime import date
+from datetime import datetime
 
 LETTERS = {
     "a": ["###", "# #", "###", "# #", "# #"],
@@ -54,131 +55,71 @@ def gen_letters(text):
     bigletters = [LETTERS.get(i.lower(), LETTERS[" "]) for i in text]
     return "\n".join(["  ".join([b[l] for b in bigletters]) for l in range(5)])
 
-def hours_to(countdown):
-    return str(divmod(countdown.total_seconds(), 3600))
+def hours(countdown):
+    return __hours_div_mod(countdown)[0]
 
-def minutes_to(countdown):
-    return str(divmod(countdown.total_seconds(), 60))
+def __hours_div_mod(countdown):
+    return divmod(countdown.seconds, 3600)
 
+def minutes(countdown):
+    return divmod( __hours_div_mod(countdown)[1], 60)[0]
 
-def getDuration(then, now=date.today(), interval="default"):
-    # Returns a duration as specified by variable interval
-    # Functions, except totalDuration, returns [quotient, remainder]
-
-    duration = now - then  # For build-in functions
-    duration_in_s = duration.total_seconds()
-
-    def years():
-        return divmod(duration_in_s, 31536000)  # Seconds in a year=31536000.
-
-    def days(seconds=None):
-        return divmod(seconds if seconds != None else duration_in_s, 86400)  # Seconds in a day = 86400
-
-    def hours(seconds=None):
-        return divmod(seconds if seconds != None else duration_in_s, 3600)  # Seconds in an hour = 3600
-
-    def minutes(seconds=None):
-        return divmod(seconds if seconds != None else duration_in_s, 60)  # Seconds in a minute = 60
-
-    def seconds(seconds=None):
-        if seconds != None:
-            return divmod(seconds, 1)
-        return duration_in_s
-
-    def totalDuration():
-        y = years()
-        d = days(y[1])  # Use remainder to calculate next variable
-        h = hours(d[1])
-        m = minutes(h[1])
-        s = seconds(m[1])
-
-        return "Time between dates: {} years, {} days, {} hours, {} minutes and {} seconds".format(int(y[0]), int(d[0]),
-                                                                                                   int(h[0]), int(m[0]),
-                                                                                                   int(s[0]))
-
-    return {
-        'years': int(years()[0]),
-        'days': int(days()[0]),
-        'hours': int(hours()[0]),
-        'minutes': int(minutes()[0]),
-        'seconds': int(seconds()),
-        'default': totalDuration()
-    }[interval]
-
-
-
-
+def seconds(countdown):
+    return divmod( __hours_div_mod(countdown)[1], 60)[1]
 
 def main():
 
-    today = date.today()
-    christmas = date(today.year, 12, 25)
+    today = datetime.today()
+    christmas = datetime(today.year, 12, 25)
 
     countdown = christmas - today
 
-    print(countdown)
+    hours_until = str(hours(countdown))
+    minutes_until = str(minutes(countdown))
+    seconds_until = str(seconds(countdown))
 
+    print(gen_letters(hours_until + "-" + minutes_until + "-" + seconds_until))
 
-    hours = hours_to(countdown)
-    mins = minutes_to(countdown)
-
-    # Example usage
-    # then = date(2012, 3, 5, 23, 8, 15)
-    # now = date.today()
-    #
-    # print(getDuration(then))  # E.g. Time between dates: 7 years, 208 days, 21 hours, 19 minutes and 15 seconds
-    # print(getDuration(then, now, 'years'))  # Prints duration in years
-    # print(getDuration(then, now, 'days'))  # days
-    # print(getDuration(then, now, 'hours'))  # hours
-    # print(getDuration(then, now, 'minutes'))  # minutes
-    # print(getDuration(then, now, 'seconds'))  #
-
-
-    until_christmas = gen_letters(str(countdown.days))
-    until_christmas = gen_letters(hours)
-    until_christmas = gen_letters(mins)
-    until_christmas = gen_letters(str(countdown.total_seconds()))
-
-    print(until_christmas)
-    print(
-""".:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:..:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:
-.                                                                                           .
-.                                                                                           .
-.                                                                                           .
-.                                                                                           .
-.                                                                                           .
-.                                                                                           .
-.                                                                                           .
-.                                                                                           .
-.                                                                                           .
-.                                                                                           .
-.                                                                                           .
-.                                                                                           .
-.                                                                                           .
-.                                                                                           .
-.                                   |                         _...._                        .
-.                                \  _  /                    .::o:::::.                      .
-.                                 (\o/)                    .:::'''':o:.                     .
-.                             ---  / \  ---                :o:_    _:::                     .
-.                                  >*<                     `:}_>()<_{:'                     .
-.                                 >0<@<                 @    `'//\\\\'`    @                  .
-.                                >>>@<<*              @ #     //  \\\\     # @                .
-.                               >@>*<0<<<           __#_#____/'____'\____#_#__              .
-.                              >*>>@<<<@<<         [__________________________]             .
-.                             >@>>0<<<*<<@<         |=_- .-/\ /\ /\ /\--. =_-|              .
-.                            >*>>0<<@<<<@<<<        |-_= | \ \\\\ \\\\ \\\\ \ |-_=-|              .
-.                           >@>>*<<@<>*<<0<*<       |_=-=| / // // // / |_=-_|              .
-.             \*/          >0>>*<<@<>0><<*<@<<      |=_- |`-'`-'`-'`-'  |=_=-|              .
-.         ___/\\\\U//___    >*>>@><0<<*>>@><*<0<<     | =_-| o          o |_==_|              .
-.         |\\\\ | | \\\\|    >@>>0<*<<0>>@<<0<<<*<@<    |=_- | !     (    ! |=-_=|              .
-.         | \\\\| | _(UU)_ >((*))_>0><*<0><@<<<0<*<  _|-,-=| !    ).    ! |-_-=|_             .
-.         |\ \| || / //||.*.*.*.|>>@<<*<<@>><0<<@</=-((=_| ! __(:')__ ! |=_==_-\            .
-.         |\\\\_|_|&&_// ||*.*.*.*|_\\db//__     (\_/)-=))-|/^\=^=^^=^=/^\| _=-_-_\            .
-.         \"\"\"\"|\'.\'.\'|~~|.*.*.*|     ____|_   =(\'.\')=//   ,------------.                     .
-.             '.\'.\'.|   ^^^^^^|____|>>>>>>|  ( ~~~ )/   (((((((())))))))                    .
-.             ~~~~~~~~         \"\"\"\"\'------'  \'w---w\'     \'------------\'                     .
-.                                                                                           .
-.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:.""")
+    # print(gen_letters(str(countdown)))
+#     print(
+# """.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:..:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:
+# .                                                                                           .
+# .                                                                                           .
+# .                                                                                           .
+# .                                                                                           .
+# .                                                                                           .
+# .                                                                                           .
+# .                                                                                           .
+# .                                                                                           .
+# .                                                                                           .
+# .                                                                                           .
+# .                                                                                           .
+# .                                                                                           .
+# .                                                                                           .
+# .                                                                                           .
+# .                                   |                         _...._                        .
+# .                                \  _  /                    .::o:::::.                      .
+# .                                 (\o/)                    .:::'''':o:.                     .
+# .                             ---  / \  ---                :o:_    _:::                     .
+# .                                  >*<                     `:}_>()<_{:'                     .
+# .                                 >0<@<                 @    `'//\\\\'`    @                  .
+# .                                >>>@<<*              @ #     //  \\\\     # @                .
+# .                               >@>*<0<<<           __#_#____/'____'\____#_#__              .
+# .                              >*>>@<<<@<<         [__________________________]             .
+# .                             >@>>0<<<*<<@<         |=_- .-/\ /\ /\ /\--. =_-|              .
+# .                            >*>>0<<@<<<@<<<        |-_= | \ \\\\ \\\\ \\\\ \ |-_=-|              .
+# .                           >@>>*<<@<>*<<0<*<       |_=-=| / // // // / |_=-_|              .
+# .             \*/          >0>>*<<@<>0><<*<@<<      |=_- |`-'`-'`-'`-'  |=_=-|              .
+# .         ___/\\\\U//___    >*>>@><0<<*>>@><*<0<<     | =_-| o          o |_==_|              .
+# .         |\\\\ | | \\\\|    >@>>0<*<<0>>@<<0<<<*<@<    |=_- | !     (    ! |=-_=|              .
+# .         | \\\\| | _(UU)_ >((*))_>0><*<0><@<<<0<*<  _|-,-=| !    ).    ! |-_-=|_             .
+# .         |\ \| || / //||.*.*.*.|>>@<<*<<@>><0<<@</=-((=_| ! __(:')__ ! |=_==_-\            .
+# .         |\\\\_|_|&&_// ||*.*.*.*|_\\db//__     (\_/)-=))-|/^\=^=^^=^=/^\| _=-_-_\            .
+# .         \"\"\"\"|\'.\'.\'|~~|.*.*.*|     ____|_   =(\'.\')=//   ,------------.                     .
+# .             '.\'.\'.|   ^^^^^^|____|>>>>>>|  ( ~~~ )/   (((((((())))))))                    .
+# .             ~~~~~~~~         \"\"\"\"\'------'  \'w---w\'     \'------------\'                     .
+# .                                                                                           .
+# .:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:.""")
 
 if __name__ == '__main__':
     main()
